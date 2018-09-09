0d9345fc7272891b8814b4bdcc261bad01d26ff7
Owen <qcgzxw@qq.com>
2018-09-09 09:46:09 +0800

    腾讯云OCR配置文件（需自行更改）
    https://cloud.tencent.com/document/product/866/17734

---
+++ apps/txystsb/conf.py
@@ -0,0 +1,13 @@
+app_id = '12520*****'
+secert_id = '**********************************'
+secert_key = '**********************************'
+bucket = 'qcgzxw'
+urls = (
+        'https://recognition.image.myqcloud.com/ocr/idcard',
+        'https://recognition.image.myqcloud.com/ocr/businesscard',
+        'https://recognition.image.myqcloud.com/ocr/drivinglicence',
+        'https://recognition.image.myqcloud.com/ocr/plate',
+        'https://recognition.image.myqcloud.com/ocr/bankcard',
+        'https://recognition.image.myqcloud.com/ocr/bizlicense',
+        'https://recognition.image.myqcloud.com/ocr/general',
+        'https://recognition.image.myqcloud.com/ocr/handwriting')
